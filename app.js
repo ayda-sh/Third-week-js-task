ae76a5a9eed3e3781b51b8f02f37427618dc8bfd
ayda <aydashamma4@gmail.com>
2025-02-06 12:01:36 -0800

    completed task 8

--- app.js
+++ app.js
@@ -22,3 +22,47 @@ btn.addEventListener('click', function() {
     changeBg.classList.add('bg-color'); 
 });
 
+
+// 8 generates a card element
+
+const formEl = document.querySelector("form")
+const cardContainer = document.querySelector(".card-container")
+
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const titleInput = formEl.querySelector("input")
+    const titleInputValue = titleInput.value
+    const contentInput = formEl.querySelector("textarea")
+    const contentInputValue = contentInput.value
+
+    createCard(titleInputValue, contentInputValue)
+})
+
+function createCard(title, content) {
+    const cardBox = document.createElement("div")
+    cardBox.classList.add("box")
+
+    const h3El = document.createElement("h3")
+    h3El.classList.add("title")
+
+    const contentEl = document.createElement("p")
+
+    const deleteBtnEL = document.createElement("button")
+
+
+
+    h3El.textContent = title
+    contentEl.textContent = content
+    deleteBtnEL.textContent = "Delete"
+
+    cardContainer.appendChild(cardBox)
+    cardBox.append(h3El, contentEl, deleteBtnEL)
+
+
+    deleteBtnEL.addEventListener("click", (e) => {
+        const card = e.target.parentElement
+        card.remove()
+    })
+    
+}
